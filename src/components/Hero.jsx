28e0ff3a85e0c83ebe7f3e3093edd2ad1c827fa7
Fikresilase <shitubilbila@gmail.com>
2024-09-14 22:47:39 +0300

    hero section complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,7 +46,6 @@ const Hero = () => {
           <BackgroundCircles />
         </div>
 
-        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
 
       <BottomLine />
